7a2143151965ab63f77c34433159064d8b0c072d
afrazier01 <amfrazier01@gmail.com>
2023-04-01 09:20:29 -0500

    Completed task 1 by adding user prompts. Working on displaying messages to screen properly

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log('The button was clicked')
 
-  passwordText.value = password;
+  //Prompt user for password criteria
+  var passwordLength = prompt('Choose password length:')
+  if (passwordLength < 8 || passwordLength > 128) {
+    console.error('password must be at least 8 characters and no more than 128 characters')
+
+    return
+  }
+
+  
+  var lowercasePrompt = prompt('Include Lowercase?')
+  var lowercasePrompt = prompt('Include Uppercase?')
+  var lowercasePrompt = prompt('Include Number?')
+  var lowercasePrompt = prompt('Include Special Characters?')
+  
+  
+  console.log('User selected',passwordLength,'for the password length.')
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
