59a13a1916ee55faa9ecbd5398cc1efebadb2f19
afrazier01 <amfrazier01@gmail.com>
2023-04-02 01:43:26 -0500

    Completed code required for acceptance criteria

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   console.log('The button was clicked')
-
+  randomPassword = ''
+  passwordCriteria = ''
+ 
   //Prompt user for password criteria
   var passwordLength = prompt('Choose password length:')
   if (passwordLength < 8 || passwordLength > 128) {
-    console.error('password must be at least 8 characters and no more than 128 characters')
-
+    console.error('User input in not within valid range')
+    alert('Invalid Value: Password must be in between 8 and 128 characters.' )
     return
   }
 
-  
-  var lowercasePrompt = prompt('Include Lowercase?')
-  var lowercasePrompt = prompt('Include Uppercase?')
-  var lowercasePrompt = prompt('Include Number?')
-  var lowercasePrompt = prompt('Include Special Characters?')
-  
-  
-  console.log('User selected',passwordLength,'for the password length.')
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  var lowercasePrompt = prompt('Do you want to include lowercase characters?')
+  if (lowercasePrompt === 'yes') {
+    passwordCriteria += 'abcdefghijklmnopqrstuvwuxyz'
+  }
+  var uppercasePrompt = prompt('Do you want to include uppercase characters?')
+  if (lowercasePrompt === 'yes') {
+    passwordCriteria += 'ABCDEFGHIJKLMNOPQRSTUVWUXYZ'
+  }
+  var numbersPrompt = prompt('Do you want to include numbers?')
+  if (lowercasePrompt === 'yes') {
+    passwordCriteria += '0123456789'
+  }
+  var specialChartactersPrompt = prompt('Do you want to include special characters?')
+  if (lowercasePrompt === 'yes') {
+    passwordCriteria += '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+  }
 
-  // passwordText.value = password;
+  var userConfirm = confirm('Are you sure you have entered the correct password requirements?')
+  if (userConfirm === false) {
+    event.preventDefault()
+  }
+  if (passwordCriteria === '') {
+    alert('You did not enter any criteria. Please try again')
+    return
+  }
+  
+  //Log user inputs to console
+  console.log('User selected',passwordLength,'for the password length\n They selected:',lowercasePrompt,'for lower case characters\n They selected:',uppercasePrompt,'for upper case characters\n They selected:',numbersPrompt,'for numbers\n They selected:',specialChartactersPrompt,'for special characters')
 
+  //generate password
+  for (var i = 0; i < passwordLength; i++) {
+    randomPassword += passwordCriteria.charAt(Math.floor(Math.random() * passwordCriteria.length))
+//https://www.youtube.com/watch?v=jww3V2fSQyg&t=190s
+  }                             
+  
+  console.log(randomPassword)
+  document.getElementById('password').innerHTML = randomPassword
 }
 
 // Add event listener to generate button
